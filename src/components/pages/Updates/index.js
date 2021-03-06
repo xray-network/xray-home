b2376e39726372e3cf9965a584d9d1ade0d451ed
Ray Robot <hello@rraayy.com>
2021-03-06 20:57:50 +0200

    roadmap / layout improvements

--- src/components/pages/Updates/index.js
+++ src/components/pages/Updates/index.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react"
+import { Tooltip } from "antd"
+import classnames from 'classnames'
 import { format } from 'date-fns'
 import { Table } from "antd"
 
@@ -53,6 +55,92 @@ const repos = [
   'awesome-cardano-pools',
 ]
 
+const dev = [
+  {
+    status: 'active',
+    title: 'RnD Phase',
+    date: 'Lifetime',
+  },
+  {
+    status: 'completed',
+    title: 'Ray Website Portal',
+    date: 'Q1 2021',
+  },
+  {
+    status: 'completed',
+    title: 'Pools Environment Setup',
+    date: 'Q1 2021',
+  },
+  {
+    status: 'completed',
+    title: 'Light Wallet Frontend',
+    date: 'Q1 2021',
+  },
+  {
+    status: 'active',
+    title: 'Wallet Send/Receive',
+    date: 'Q1 2021',
+  },
+  {
+    status: 'active',
+    title: 'Wallet Stake',
+    date: 'Q1 2021',
+  },
+  {
+    status: 'active',
+    title: 'Wallet Rewards',
+    date: 'Q1 2021',
+  },
+  {
+    status: 'pending',
+    title: 'Chrome Extension',
+    date: 'Q2 2021',
+  },
+  {
+    status: 'pending',
+    title: 'Wallet Swap',
+    date: 'Q2-Q3 2021',
+  },
+  {
+    status: 'pending',
+    title: 'Wallet KickStart',
+    date: 'Q2-Q3 2021',
+  },
+  {
+    status: 'pending',
+    title: 'Wallet NFT Marketplace',
+    date: 'Q3 2021',
+  },
+  {
+    status: 'pending',
+    title: 'Data Containers',
+    date: '2022',
+  },
+]
+
+const apps = [
+  {
+    status: 'pending',
+    title: 'Windows App Electron',
+    date: 'Q2 2021',
+  },
+  {
+    status: 'pending',
+    title: 'macOS App Electron',
+    date: 'Q2 2021',
+  },
+  {
+    status: 'pending',
+    title: 'iOS App Cordova',
+    date: 'Q3 2021',
+  },
+  {
+    status: 'pending',
+    title: 'Android App Cordova',
+    date: 'Q3 2021',
+  },
+]
+
 let fetchedData = []
 
 export default () => {
@@ -61,20 +149,20 @@ export default () => {
 
   useEffect(() => {
     fetchedData = []
-    Promise.all(repos.map(repo => {
-      return fetch(`https://api.github.com/repos/ray-network/${repo}/commits?page=1&per_page=50`)
-        .then(res => res.json())
-        .then((result => {
-          fetchedData.push({
-            repo,
-            data: result,
-          })
-        }))
-    }))
-      .then(() => {
-        setLoading(false)
-        formatData()
-      })
+    // Promise.all(repos.map(repo => {
+    //   return fetch(`https://api.github.com/repos/ray-network/${repo}/commits?page=1&per_page=50`)
+    //     .then(res => res.json())
+    //     .then((result => {
+    //       fetchedData.push({
+    //         repo,
+    //         data: result,
+    //       })
+    //     }))
+    // }))
+    //   .then(() => {
+    //     setLoading(false)
+    //     formatData()
+    //   })
     // eslint-disable-next-line
   }, [])
 
@@ -104,6 +192,52 @@ export default () => {
   return (
     <div className="ray__block">
       <div className="container-fluid">
+        <h2 className="ray__heading mb-4">Development Roadmap</h2>
+        <div className="mb-3">
+          <h5 className="mb-3">Web Services</h5>
+          <div className={style.roadmap}>
+            {dev.map((item, index) => {
+              return (
+                <Tooltip key={index} title={<div className="text-capitalize">{item.status}</div>}>
+                  <div className={classnames({
+                    [style.item]: true,
+                    [style.itemActive]: item.status === 'active',
+                    [style.itemCompleted]: item.status === 'completed',
+                  })}>
+                    <span className={style.itemIcon}>
+                      {item.status === 'completed' && <i className="fe fe-check-circle" />}
+                      {item.status === 'active' && <i className="fe fe-activity" />}
+                    </span>
+                    <span className={style.itemDate}>{item.date}</span>
+                    <span className={style.itemTitle}>{item.title}</span>
+                  </div>
+                </Tooltip>
+              )
+            })}
+          </div>
+        </div>
+        <div className="mb-5">
+          <h5 className="mb-3">Apps</h5>
+          <div className={style.roadmap}>
+            {apps.map((item, index) => {
+              return (
+                <Tooltip key={index} title={<div className="text-capitalize">{item.status}</div>}>
+                  <div className={classnames({
+                    [style.item]: true,
+                    [style.itemActive]: item.status === 'active',
+                    [style.itemCompleted]: item.status === 'completed',
+                  })}>
+                    <span className={style.itemIcon}>
+                      {item.status === 'completed' && <i className="fe fe-check-circle" />}
+                    </span>
+                    <span className={style.itemDate}>{item.date}</span>
+                    <span className={style.itemTitle}>{item.title}</span>
+                  </div>
+                </Tooltip>
+              )
+            })}
+          </div>
+        </div>
         <h2 className="ray__heading mb-4">Development Updates</h2>
         <div className="mb-5">
           <p>
